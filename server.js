9ce007a36cebfcaca8db37a9ede0b17015edb557
Olga Umrikhina <zetaumr@gmail.com>
2026-05-07 16:51:37 +0300

    feat: add delete route

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const meters = require('./meters.json');
 const areas = require('./areas.json');
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
+
 app.use(express.json());
 
 // GET /api/v4/test/meters/
