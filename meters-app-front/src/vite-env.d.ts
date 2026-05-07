a32cb95e7e9aeac440744909dc552512843371d2
Olga Umrikhina <zetaumr@gmail.com>
2026-05-07 18:44:34 +0300

    fix:  setting env deploy

--- meters-app-front/src/vite-env.d.ts
+++ meters-app-front/src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_HOST?: string;
+  readonly VITE_BASE_PATH?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
